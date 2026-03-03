b4ccf74fbe23de2aa0cdeebdcb96cbf06930da8d
psharma-03 <psk.fr.03@gmail.com>
2026-03-03 15:01:31 +0100

    fix: Suppress persistent linter errors in passive_monitor.py

--- modules/passive_monitor.py
+++ modules/passive_monitor.py
@@ -8,8 +8,8 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-from utils.logger import get_logger
-from utils.result_handler import create_result
+from utils.logger import get_logger # type: ignore
+from utils.result_handler import create_result # type: ignore
 
 logger = get_logger()
 
